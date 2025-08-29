345edbab4b8f6c371925f1effda78130a90d44d9
Hrishii Gautam <rishi.g1987@gmail.com>
2025-08-29 12:19:56 +0530

    Fix missing duration-600 Tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+        '600': '600ms',
+      },
+    },
   },
   plugins: [],
 };
